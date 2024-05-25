3e901992970df99c8157b8c434663b4c0b4a7404
GianLobaton <113273775+GianLobaton@users.noreply.github.com>
2024-05-25 15:00:14 +0800

    Add files via upload

---
+++ chattyyy.sql
@@ -0,0 +1,8 @@
+-- CREATE TABLE chatroom(username TEXT NOT NULL, msg TEXT NOT NULL);
+INSERT INTO chatroom(username,msg)
+VALUES ("PAULA", "HIII"),
+       ("Marie", "hello"),
+       ("bonifacio","t"),
+       ("bonifacio","e"),
+       ("Mendoza","oy");
+SELECT * FROM chatroom;
